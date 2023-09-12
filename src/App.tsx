0fae9c385d164775e530b07fd2318676370f6d92
eunchokang <wkdb0425@gmail.com>
2023-09-12 13:43:28 +0900

    chore: change lint settings (#8)

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import AppRoutes from './router/AppRoutes';
 import { AppLayout } from './styles/AppLayout';
-import { GlobalStyle } from './styles/GlobalStyle';
+import { GlobalStyle } from './styles/GlobalStyle.js';
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <div className='App'>오늘의 메뉴 아자자! // 화이팅입니다~~!~!~!~!~!🫶</div> */}
       <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyle />
       <AppLayout>
