ee9c0db79c8779e60fba6bfc30a418a5c3561315
dasom im <bringvotrevin@gmail.com>
2023-09-14 04:18:19 +0900

    feat: calculate mobile height (#5)

--- src/styles/AppLayout.jsx
+++ src/styles/AppLayout.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const AppLayout = styled.div`
   max-width: 393px;
-  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
   margin: 0 auto;
 
   // 레이아웃 확인 위한 테스트코드. 삭제예정
