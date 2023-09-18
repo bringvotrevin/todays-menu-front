8c4dec32ee09b9dd3337181082d537d9a0a0086b
eunchokang <wkdb0425@gmail.com>
2023-09-18 18:11:12 +0900

    refactor: separate ShareBottomSheet as common component (#25)

--- src/pages/Vote/Vote.tsx
+++ src/pages/Vote/Vote.tsx
@@ -1,29 +1,26 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-// import MenuCard from 'components/common/MenuCard/MenuCard';
-import Button from 'components/common/Button/Button';
-
 import * as S from './Vote.styled';
-import shareViaLinkImg from 'assets/icons/icon-share-link.svg';
-import shareViaKakaoImg from 'assets/icons/icon-kakaotalk.svg';
+import Button from 'components/common/Button/Button';
+import ShareBottomSheet from 'components/common/modal/ShareBottomSheet';
+// import MenuCard from 'components/common/MenuCard/MenuCard';
 
 const Vote = () => {
-  const [modalOn, setModalOn] = useState<boolean>(false);
+  const [IsModalOn, setIsModalOn] = useState<boolean>(false);
   const navigate = useNavigate();
 
   const handleSubmit = () => {
     navigate('/random-menu/1/result');
   };
 
-  const handleShareClick = () => {
-    setModalOn(true);
+  const handleModalClick = () => {
+    setIsModalOn(true);
   };
 
   const handleModalClose = (event: React.MouseEvent<HTMLDivElement>) => {
     event.stopPropagation();
     event.preventDefault();
-    setModalOn(false);
+    setIsModalOn(false);
   };
 
   return (
@@ -43,28 +40,14 @@ const Vote = () => {
             <Button className="renewList" $variant="white">
               초기화
             </Button>
-            <Button className="makeNewList" $variant="orange" onClick={handleShareClick}>
+            <Button className="makeNewList" $variant="orange" onClick={handleModalClick}>
               투표 만들기
             </Button>
           </S.BtnGroup>
         </S.Wrapper>
       </S.Layout>
       {/* 모달은 포탈 써서 전역으로 나중에 바꿀게요!! */}
-      {modalOn && (
-        <S.VoteBottomSheet handleModalClose={handleModalClose}>
-          <ul>
-            <li className="shareViaKaKao">
-              <img src={shareViaKakaoImg} alt="share via kakaotalk icon" />
-              <p>카카오톡으로 공유하기</p>
-            </li>
-            <S.Line />
-            <li className="shareViaLink">
-              <img src={shareViaLinkImg} alt="share via link icon" />
-              <p>링크 복사하기</p>
-            </li>
-          </ul>
-        </S.VoteBottomSheet>
-      )}
+      {IsModalOn && <ShareBottomSheet handleModalClose={handleModalClose} />}
     </>
   );
 };
