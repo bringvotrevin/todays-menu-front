f91ad00dcf11d3aeb03240b697e367e62c8b008a
eunchokang <wkdb0425@gmail.com>
2023-09-18 17:24:34 +0900

    design: design OverallRanking page (#25)

--- src/pages/OverallRanking/OverallRanking.styled.ts
+++ src/pages/OverallRanking/OverallRanking.styled.ts
@@ -5,3 +5,96 @@ export const Wrapper = styled.div`
   height: 100%;
   background-color: var(--color-main-orange);
 `;
+
+export const OverallRankingWrapper = styled.div`
+  padding: 45px 19px 43px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+
+  .page-title {
+    margin-bottom: 3px;
+    font-size: 30px;
+    font-weight: var(--font-bold);
+    color: #fff;
+    text-align: center;
+  }
+`;
+
+export const RestaurantList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+`;
+
+export const RestaurantItem = styled.li`
+  width: 100%;
+  background-color: #fff;
+  padding: 20px 22px;
+  display: flex;
+  justify-content: space-between;
+  border-radius: 10px;
+`;
+
+export const Ranking = styled.div`
+  padding: 0 2px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  img {
+    width: 16px;
+  }
+
+  .ranking {
+    /* margin: 1px 0 5px; */
+    display: block;
+    font-size: 23px;
+    font-weight: var(--font-bold);
+    line-height: 1.5;
+  }
+
+  .total-number {
+    display: inline-block;
+    color: #c2c2c2;
+    /* line-height: 1.7; */
+  }
+`;
+
+export const RestaurantData = styled.div`
+  width: 60%;
+  line-height: 1.7;
+  padding-top: 4px;
+
+  .name {
+    font-size: var(--md);
+    font-weight: var(--font-bold);
+    margin-right: 10px;
+  }
+  .distance {
+    display: inline;
+    color: var(--color-sub-gray);
+  }
+
+  .tags {
+    display: flex;
+    gap: 5px;
+    color: var(--color-main-orange);
+  }
+`;
+
+export const ButtonShare = styled.button`
+  display: block;
+  background-color: rgba(255, 255, 255, 0.2);
+  color: #fff;
+  padding: 11px 48px;
+  border-radius: 20px;
+  margin: 0 auto;
+
+  img {
+    margin-right: 10px;
+    vertical-align: bottom;
+  }
+`;
--- src/pages/OverallRanking/OverallRanking.tsx
+++ src/pages/OverallRanking/OverallRanking.tsx
@@ -1,6 +1,114 @@
 import React from 'react';
 import * as S from './OverallRanking.styled';
+import toDetail from 'assets/icons/btn-link-gray.svg';
+import shareResult from 'assets/icons/icon-share-resut.svg';
+import Button from 'components/common/Button/Button';
+import winner from 'assets/icons/icon-winner.svg';
 
 export default function OverallRanking() {
-  return <S.Wrapper>OverallRanking</S.Wrapper>;
+  return (
+    <S.Wrapper>
+      <S.OverallRankingWrapper>
+        <h3 className="page-title">전체 순위</h3>
+        <S.RestaurantList>
+          <S.RestaurantItem>
+            <S.Ranking>
+              <img src={winner} alt="winner icon" />
+              <strong className="ranking">1등</strong>
+              <p>
+                5<span className="total-number">/15명</span>
+              </p>
+            </S.Ranking>
+            <S.RestaurantData>
+              <strong className="name">오제제 강남점</strong>
+              <p className="distance">120m</p>
+              <div className="tags">
+                <p># 양식</p>
+                <p># 스테이크, 립</p>
+              </div>
+            </S.RestaurantData>
+            <img src={toDetail} alt="more detail icon" />
+          </S.RestaurantItem>
+          {/* temp item */}
+          <S.RestaurantItem>
+            <S.Ranking>
+              {/* <img src={winner} alt="winner icon" /> */}
+              <strong className="ranking">1등</strong>
+              <p>
+                5<span className="total-number">/15명</span>
+              </p>
+            </S.Ranking>
+            <S.RestaurantData>
+              <strong className="name">오제제 강남점</strong>
+              <p className="distance">120m</p>
+              <div className="tags">
+                <p># 양식</p>
+                <p># 스테이크, 립</p>
+              </div>
+            </S.RestaurantData>
+            <img src={toDetail} alt="more detail icon" />
+          </S.RestaurantItem>
+          <S.RestaurantItem>
+            <S.Ranking>
+              {/* <img src={winner} alt="winner icon" /> */}
+              <strong className="ranking">1등</strong>
+              <p>
+                5<span className="total-number">/15명</span>
+              </p>
+            </S.Ranking>
+            <S.RestaurantData>
+              <strong className="name">오제제 강남점</strong>
+              <p className="distance">120m</p>
+              <div className="tags">
+                <p># 양식</p>
+                <p># 스테이크, 립</p>
+              </div>
+            </S.RestaurantData>
+            <img src={toDetail} alt="more detail icon" />
+          </S.RestaurantItem>
+          <S.RestaurantItem>
+            <S.Ranking>
+              {/* <img src={winner} alt="winner icon" /> */}
+              <strong className="ranking">1등</strong>
+              <p>
+                5<span className="total-number">/15명</span>
+              </p>
+            </S.Ranking>
+            <S.RestaurantData>
+              <strong className="name">오제제 강남점</strong>
+              <p className="distance">120m</p>
+              <div className="tags">
+                <p># 양식</p>
+                <p># 스테이크, 립</p>
+              </div>
+            </S.RestaurantData>
+            <img src={toDetail} alt="more detail icon" />
+          </S.RestaurantItem>
+          <S.RestaurantItem>
+            <S.Ranking>
+              <strong className="ranking">1등</strong>
+              <p>
+                5<span className="total-number">/15명</span>
+              </p>
+            </S.Ranking>
+            <S.RestaurantData>
+              <strong className="name">오제제 강남점</strong>
+              <p className="distance">120m</p>
+              <div className="tags">
+                <p># 양식</p>
+                <p># 스테이크, 립</p>
+              </div>
+            </S.RestaurantData>
+            <img src={toDetail} alt="more detail icon" />
+          </S.RestaurantItem>
+          {/* temp item */}
+          <S.ButtonShare>
+            <img src={shareResult} alt="share result icon" />
+            공유하기
+          </S.ButtonShare>
+          <Button>다시하기</Button>
+        </S.RestaurantList>
+      </S.OverallRankingWrapper>
+    </S.Wrapper>
+  );
 }
