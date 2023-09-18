f33de689897d16177abfdf9afe1423c1dd94bc57
eunchokang <wkdb0425@gmail.com>
2023-09-18 23:48:35 +0900

    feat: connect api (#27)

--- src/pages/OverallRanking/OverallRanking.tsx
+++ src/pages/OverallRanking/OverallRanking.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as S from './OverallRanking.styled';
 import ShareBottomSheet from 'components/common/modal/ShareBottomSheet';
 import Button from 'components/common/Button/Button';
 import toDetail from 'assets/icons/btn-link-gray.svg';
 import shareResult from 'assets/icons/icon-share-resut.svg';
 import winner from 'assets/icons/icon-winner.svg';
+import { useGetResult } from 'apis/query/useGetResult';
 
 export default function OverallRanking() {
   const [IsModalOn, setIsModalOn] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const resultData = useGetResult();
+  console.log('resultData', resultData);
 
   const handleModalClick = () => {
     setIsModalOn(true);
@@ -19,108 +25,43 @@ export default function OverallRanking() {
     setIsModalOn(false);
   };
 
+  const handleRetry = () => {
+    navigate('/random-menu');
+  };
+
   return (
     <>
       <S.Wrapper>
         <S.OverallRankingWrapper>
           <h3 className="page-title">전체 순위</h3>
           <S.RestaurantList>
-            <S.RestaurantItem>
-              <S.Ranking>
-                <img src={winner} alt="winner icon" />
-                <strong className="ranking">1등</strong>
-                <p>
-                  5<span className="total-number">/15명</span>
-                </p>
-              </S.Ranking>
-              <S.RestaurantData>
-                <strong className="name">오제제 강남점</strong>
-                <p className="distance">120m</p>
-                <div className="tags">
-                  <p># 양식</p>
-                  <p># 스테이크, 립</p>
-                </div>
-              </S.RestaurantData>
-              <img src={toDetail} alt="more detail icon" />
-            </S.RestaurantItem>
-            {/* temp items start */}
-            <S.RestaurantItem>
-              <S.Ranking>
-                {/* <img src={winner} alt="winner icon" /> */}
-                <strong className="ranking">1등</strong>
-                <p>
-                  5<span className="total-number">/15명</span>
-                </p>
-              </S.Ranking>
-              <S.RestaurantData>
-                <strong className="name">오제제 강남점</strong>
-                <p className="distance">120m</p>
-                <div className="tags">
-                  <p># 양식</p>
-                  <p># 스테이크, 립</p>
-                </div>
-              </S.RestaurantData>
-              <img src={toDetail} alt="more detail icon" />
-            </S.RestaurantItem>
-            <S.RestaurantItem>
-              <S.Ranking>
-                {/* <img src={winner} alt="winner icon" /> */}
-                <strong className="ranking">1등</strong>
-                <p>
-                  5<span className="total-number">/15명</span>
-                </p>
-              </S.Ranking>
-              <S.RestaurantData>
-                <strong className="name">오제제 강남점</strong>
-                <p className="distance">120m</p>
-                <div className="tags">
-                  <p># 양식</p>
-                  <p># 스테이크, 립</p>
-                </div>
-              </S.RestaurantData>
-              <img src={toDetail} alt="more detail icon" />
-            </S.RestaurantItem>
-            <S.RestaurantItem>
-              <S.Ranking>
-                {/* <img src={winner} alt="winner icon" /> */}
-                <strong className="ranking">1등</strong>
-                <p>
-                  5<span className="total-number">/15명</span>
-                </p>
-              </S.Ranking>
-              <S.RestaurantData>
-                <strong className="name">오제제 강남점</strong>
-                <p className="distance">120m</p>
-                <div className="tags">
-                  <p># 양식</p>
-                  <p># 스테이크, 립</p>
-                </div>
-              </S.RestaurantData>
-              <img src={toDetail} alt="more detail icon" />
-            </S.RestaurantItem>
-            <S.RestaurantItem>
-              <S.Ranking>
-                <strong className="ranking">1등</strong>
-                <p>
-                  5<span className="total-number">/15명</span>
-                </p>
-              </S.Ranking>
-              <S.RestaurantData>
-                <strong className="name">오제제 강남점</strong>
-                <p className="distance">120m</p>
-                <div className="tags">
-                  <p># 양식</p>
-                  <p># 스테이크, 립</p>
-                </div>
-              </S.RestaurantData>
-              <img src={toDetail} alt="more detail icon" />
-            </S.RestaurantItem>
-            {/* temp items end */}
+            {resultData?.slice(0, 5).map((item: any) => (
+              <S.RestaurantItem key={item.id}>
+                <S.Ranking>
+                  <img src={winner} alt="winner icon" />
+                  <strong className="ranking">{item.rank}등</strong>
+                  <p>
+                    {item.count}
+                    <span className="total-number">/15명</span>
+                  </p>
+                </S.Ranking>
+                <S.RestaurantData>
+                  <strong className="name">{item.title}</strong>
+                  <p className="distance">{item.distance}m</p>
+                  <div className="tags">
+                    <p># {item.category}</p>
+                    <p># 스테이크, 립</p>
+                  </div>
+                </S.RestaurantData>
+                <img src={toDetail} alt="more detail icon" />
+              </S.RestaurantItem>
+            ))}
+
             <S.ButtonShare onClick={handleModalClick}>
               <img src={shareResult} alt="share result icon" />
               공유하기
             </S.ButtonShare>
-            <Button>다시하기</Button>
+            <Button onClick={handleRetry}>다시하기</Button>
           </S.RestaurantList>
         </S.OverallRankingWrapper>
       </S.Wrapper>
