031a8be6be6a72ddc7271c6541251b680f8ad513
dasom im <bringvotrevin@gmail.com>
2023-09-23 04:12:43 +0900

    feat: change main page to Location

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Landing from 'pages/Landing/Landing';
 import Location from 'pages/Location/Location';
 import RandomList from 'pages/RandomList/RandomList';
 import Result from 'pages/Result/Result';
@@ -10,8 +9,7 @@ import OverallRanking from 'pages/OverallRanking/OverallRanking';
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/location" element={<Location />} />
+      <Route path="/" element={<Location />} />
       <Route path="/random-menu" element={<RandomList />} />
       <Route path="/random-menu/:id" element={<Poll />} />
       <Route path="/random-menu/:id/result" element={<Result />} />
